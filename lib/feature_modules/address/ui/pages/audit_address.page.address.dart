d6103a075bd3dd721033f4be5364c5546d2efd26
Prasanth K M <prasanthkm49@gmail.com>
2024-08-27 10:48:44 +0530

    added default floor & house number

--- lib/feature_modules/address/ui/pages/audit_address.page.address.dart
+++ lib/feature_modules/address/ui/pages/audit_address.page.address.dart
@@ -410,10 +410,10 @@ class _AuditAddressPage_AddressState extends State<AuditAddressPage_Address> {
               street: addressController.streetTextEditingController.value.text,
               houseNumber:
                   int.parse(addressController.houseNumberTextEditingController.value.text.toString().trim()),
-              floorNumber:
-              int.parse(addressController.floorNumberTextEditingController.value.text.toString().trim()),
-              apartmentNo:
-              int.parse(addressController.apartmentNumberTextEditingController.value.text.toString().trim()),
+            floorNumber:addressController.floorNumberTextEditingController.value.text.toString().trim()==""?-1:
+            int.parse(addressController.floorNumberTextEditingController.value.text.toString().trim()),
+            apartmentNo:addressController.apartmentNumberTextEditingController.value.text.toString().trim()==""?-1:
+            int.parse(addressController.apartmentNumberTextEditingController.value.text.toString().trim()),
           ),
           mobile
         ]);
